6f5abbc6b1f4e85cb634ed85e9c1ed1e74aa6afa
ainhoagaarciia <104358730+ainhoagaarciia@users.noreply.github.com>
2024-10-14 16:31:59 +0200

    Update desencriptar_texto.py

--- desencriptar_texto.py
+++ desencriptar_texto.py
@@ -1,23 +1,35 @@
-import sys
+def calcular_frecuencia(texto):
+    frecuencia = {}
+    letras_validas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÑ'
+    
+    for letra in texto:
+        letra = letra.upper()  # Convertimos la letra a mayúscula para hacer la comparación
+        if letra in letras_validas:
+            if letra in frecuencia:
+                frecuencia[letra] += 1
+            else:
+                frecuencia[letra] = 1
+    return frecuencia
 
-# Comprobar que se hayan pasado exactamente dos argumentos al script
-if len(sys.argv) < 3:
-    print("Uso correcto: python modificar_texto.py <letra_a_cambiar> <letra_a_reemplazar>")
-    sys.exit()
+def descifrar_mensaje(cifrado):
+    letras_validas = 'ABCDEFGHIJKLMNOPQRSTUVWXYÑZ'
+    frecuencia_cifrado = calcular_frecuencia(cifrado)
+    letras_espanol = 'EAROINLDCUTSMPBFVQJGHXZÑYKW'
+    letras_ordenadas = sorted(frecuencia_cifrado, key=lambda letra: frecuencia_cifrado[letra], reverse=True)
+    mapeo = {}
+    for i in range(min(len(letras_espanol), len(letras_ordenadas))):  # Usamos el mínimo de ambas longitudes
+        mapeo[letras_ordenadas[i]] = letras_espanol[i]
+    mensaje_descifrado = ''
+    for letra in cifrado:
+        letra = letra.upper()  # Convertimos la letra a mayúscula para hacer la comparación
+        if letra in letras_validas:
+            mensaje_descifrado += mapeo[letra]
+        else:
+            mensaje_descifrado += letra
+    return mensaje_descifrado
 
-# Asignar los argumentos a variables de manera más clara
-letra_a_cambiar, letra_a_reemplazar = sys.argv[1], sys.argv[2]
+mensaje_cifrado = "RIJ AZKKZHC PIKCE XT ACKCUXJHX SZX, E NZ PEJXKE, PXGIK XFDKXNEQE RIPI RIPQEHCK ET OENRCNPI AXNAX ZJ RKCHXKCI AX CJAXDXJAXJRCE AX RTENX, E ACOXKXJRCE AXT RITEQIKERCIJCNPI OKXJHXDIDZTCNHE AX TE ACKXRRCIJ EJEKSZCNHE. AZKKZHC OZX ZJ OERHIK AX DKCPXK IKAXJ XJ XT DEDXT AX TE RTENX IQKXKE XJ REHETZJVE XJ GZTCI AX 1936. DXKI AZKKZHC, RIPI IRZKKX RIJ TEN DXKNIJETCAEAXN XJ TE MCNHIKCE, JI REVI AXT RCXTI. DXKNIJCOCREQE TE HKEACRCIJ KXvITZRCIJEKCE AX TE RTENX IQKXKE. NZ XJIKPX DIDZTEKCAEA XJHKX TE RTENX HKEQEGEAIKE, KXOTXGEAE XJ XT XJHCXKKI PZTHCHZACJEKCI XJ QEKRXTIJE XT 22 AX JIvCXPQKX AX 1936, PZXNHKE XNE CAXJHCOCRERCIJ. NZ PZXKHX OZX NCJ AZAE ZJ UITDX IQGXHCvI ET DKIRXNI KXvITZRCIJEKCI XJ PEKRME. NCJ AZKKZHC SZXAI PEN TCQKX XT REPCJI DEKE SZX XT XNHETCJCNPI, RIJ TE RIPDTCRCAEA AXT UIQCXKJI AXT OKXJHX DIDZTEK V AX TE ACKXRRCIJ EJEKSZCNHE, HXKPCJEKE XJ PEVI AX 1937 TE HEKXE AX TCSZCAEK TE KXvITZRCIJE, AXNPIKETCLEJAI E TE RTENX IQKXKE V OERCTCHEJAI RIJ XTTI XT DINHXKCIK HKCZJOI OKEJSZCNHE."
 
-# Leer el contenido del archivo de texto
-with open('texto.txt', 'r') as archivo:
-    texto_original = archivo.read()
+mensaje_descifrado = descifrar_mensaje(mensaje_cifrado)
 
-# Reemplazar las ocurrencias de la letra especificada
-texto_modificado = texto_original.replace(letra_a_cambiar, letra_a_reemplazar)
-
-# Guardar el texto modificado nuevamente en el archivo
-with open('texto.txt', 'w') as archivo:
-    archivo.write(texto_modificado)
-
-print(f'Se ha reemplazado "{letra_a_cambiar}" por "{letra_a_reemplazar}" en el archivo "texto.txt".')
-
+print(mensaje_descifrado)
